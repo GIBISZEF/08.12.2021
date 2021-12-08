0cef1e2788b8284dd56c9b77b82ebcafabef2f73
GIBISZEF <piaseckioskar6@gmail.com>
2021-12-08 10:35:45 +0100

    Zmiana

--- sprawdz.php
+++ sprawdz.php
@@ -1,13 +1,13 @@
 <?php 
 
-    $zmienna = $_GET['stan']
+    $zmienna = $_GET['mojstan'];
 
-    if($zmienna == 1){
+    if($zmienna==1){
         echo("zmienna: ". $zmienna);
     }else if($zmienna == 2){
         echo("zmienna inna: ". $zmienna);
     }else{
-        echo("brak danych do pobrania")
-    }
+        echo("brak danych do pobrania");
+    };
 
 ?>
